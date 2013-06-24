74532e0c122b56f9abe3555302cc67dc3b52204f
fon <fon1989@gmail.com>
2013-06-25 00:06:26 +0100

    Added a class to statically create Messages as a wraper to the builder provided by protobuf

---
+++ edpp/src/main/java/comm/MessageBuilder.java
@@ -0,0 +1,66 @@
+package comm;
+
+import comm.ProtocolMessage.Message;
+import comm.ProtocolMessage.Message.Builder;
+import comm.ProtocolMessage.Message.MessageType;
+
+public class MessageBuilder {
+
+	public static Message buildNewMessage(int numOfExecutions, int numOfRounds) {
+		Message m =
+				Message.newBuilder()
+				.setType(MessageType.NEW)
+				.setExecution(numOfExecutions)
+				.setRound(numOfRounds)
+				.build();
+		
+		return m;
+	}
+	
+	public static Message buildInitMessage(String nodeId, String sessionId, 
+			int numOfExecutions, int numOfRounds, double value) {
+		Message m = 
+				Message.newBuilder()
+				.setNodeId(nodeId)
+				.setType(MessageType.INIT)
+				.setSession(sessionId)
+				.setExecution(numOfExecutions)
+				.setRound(numOfRounds)
+				.setVal(value)
+				.build();
+				
+		return m;
+	}
+	
+	public static Message buildNextMessage(String nodeId, String sessionId, 
+			int numOfExecution, int round, double val) {
+		Message m =
+				Message.newBuilder()
+				.setNodeId(nodeId)
+				.setType(MessageType.NEXT)
+				.setSession(sessionId)
+				.setExecution(numOfExecution)
+				.setRound(round)
+				.setVal(val)
+				.build();
+		
+		return m;
+	}
+	
+	public static Message buildGossipMessage(String nodeId, String sessionId,
+			int numOfExecution, double [] eigenvalues) {
+		Builder builder =
+				Message.newBuilder()
+				.setNodeId(nodeId)
+				.setType(MessageType.GOSSIP)
+				.setSession(sessionId)
+				.setExecution(numOfExecution);
+		
+		for (int i=0; i < eigenvalues.length; i++) {
+			builder = builder.setEigenvals(i, eigenvalues[i]);
+		}
+		
+		return builder.build();
+	}
+	
+}
