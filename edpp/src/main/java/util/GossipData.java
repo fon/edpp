8e91a521a1665f5f00a6442c3dbfca5fddfa52ba
fon <fon1989@gmail.com>
2013-06-28 16:43:34 +0100

    Implemented data structure to store all the proposed eigenvalues

---
+++ edpp/src/main/java/util/GossipData.java
@@ -0,0 +1,46 @@
+package util;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.apache.commons.math.stat.descriptive.DescriptiveStatistics;
+
+
+public class GossipData {
+
+	Map<String, double[]> collectedEigenvalues;
+	
+	public GossipData() {
+		collectedEigenvalues = new ConcurrentHashMap<String, double[]>();
+	}
+	
+	public void setNewProposal(String nodeId, double [] eigenvals) {
+		collectedEigenvalues.put(nodeId, eigenvals);
+	}
+	
+	public double [] getProposal(String nodeId) {
+		return collectedEigenvalues.get(nodeId);
+	}
+	
+	public double [] computeMedianOfProposedValues() {
+		Collection<double []> values = collectedEigenvalues.values();
+		double [][] proposals = values.toArray(new double[values.size()][]);
+		double [] finalValues = new double[proposals[0].length]; 
+		
+		for (int j = 0; j < proposals[0].length; j++) {
+			double items [] = new double[proposals.length];
+			for (int i = 0; i < proposals.length; i++) {
+				items[i] = proposals[i][j];
+			}
+			finalValues[j] = findMedian(items);
+		}
+		return finalValues;
+	}
+	
+	private double findMedian(double [] items) {
+		DescriptiveStatistics stats = new DescriptiveStatistics(items);
+		return stats.getPercentile(50);
+	}
+	
+}
