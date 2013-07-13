0731a68cc6e0b86fbb1cc58efb6a9225367fc8a6
fon <fon1989@gmail.com>
2013-07-13 21:10:11 +0300

    Added javadoc for the TransferableMessage class

--- edpp/src/main/java/comm/TransferableMessage.java
+++ edpp/src/main/java/comm/TransferableMessage.java
@@ -4,28 +4,54 @@ import java.net.InetAddress;
 
 import comm.ProtocolMessage.Message;
 
+/**
+ * A protocol message along with the destination address
+ * @author Xenofon Foukas
+ *
+ */
 public class TransferableMessage {
 	
 	private Message message;
 	private InetAddress address;
 
+	/**
+	 * Class constructor
+	 * @param message The Message that will be sent to the remote node
+	 * @param address The InetAddress of the remote node
+	 */
 	public TransferableMessage(Message message, InetAddress address) {
 		this.setMessage(message);
 		this.address = address;
 	}
 
+	/**
+	 * 
+	 * @return The transfered Message
+	 */
 	public Message getMessage() {
 		return message;
 	}
 
+	/**
+	 * 
+	 * @param message The Message to be transfered
+	 */
 	public void setMessage(Message message) {
 		this.message = message;
 	}
 	
+	/**
+	 * 
+	 * @return The InetAddress of the remote node
+	 */
 	public InetAddress getAddress() {
 		return address;
 	}
 
+	/**
+	 * 
+	 * @param address The InetAddress of the remote node
+	 */
 	public void setAddress(InetAddress address) {
 		this.address = address;
 	}
