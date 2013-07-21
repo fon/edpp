7966a27c2ee90028c92bc0eb1165f0140e037bde
fon <fon1989@gmail.com>
2013-07-21 23:18:22 +0300

    Added tests for EvaluationResults class

---
+++ edpp/src/test/java/evaluation/EvaluationResultsTest.java
@@ -0,0 +1,32 @@
+package evaluation;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class EvaluationResultsTest {
+
+	double [] spectralGaps = { 0.9, 0.5, 0.7, 0.1, 0.3, 1, 0.2, 0.4, 0.6, 0.8};
+	double [] mixingTimes = { 0.9, 0.5, 0.7, 0.1, 0.3, 1, 0.2, 0.4, 0.6, 0.8};
+	EvaluationResults eval;
+	
+	@Before
+	public void setUp() throws Exception {
+		eval = new EvaluationResults(0.5, 0.1);
+		for (int i = 0; i < spectralGaps.length; i++) {
+			eval.addComputedMixningTime(mixingTimes[i]);
+			eval.addComputedSpectralGap(spectralGaps[i]);
+		}
+	}
+
+	@Test
+	public void checkSpectralGapPercentError() {
+		assertEquals(50, eval.getSpectralGapPercentError(50),0);
+	}
+
+	@Test
+	public void checkMixingTimePercentError() {
+		assertEquals(900, eval.getMixingTimePercentError(100),0);
+	}
+}
