c7fc01b9af4b825efd3736e1dd3c64cfc21f6613
fon <fon1989@gmail.com>
2013-07-28 01:05:16 +0300

    Implemented a fake database for test purposes

---
+++ edpp/src/test/java/storage/FakeDatabase.java
@@ -0,0 +1,45 @@
+package storage;
+
+
+
+import java.util.Stack;
+
+import core.RecordedSession;
+import core.SessionListener;
+
+public class FakeDatabase implements Database {
+
+	private Stack<RecordedSession> storedSessions;
+	
+	public FakeDatabase() {
+		storedSessions = new Stack<RecordedSession>();
+	}
+	
+	@Override
+	public void addSession(RecordedSession rs) {
+		storedSessions.add(rs);
+	}
+
+	@Override
+	public RecordedSession getLastRecordedSession() {
+		if (storedSessions.isEmpty())
+			return null;
+		return storedSessions.peek();
+	}
+
+	@Override
+	public void addSessionListener(SessionListener listener) {
+		return;
+	}
+
+	@Override
+	public boolean removeSessionListener(SessionListener listener) {
+		return true;
+	}
+
+	@Override
+	public void closeDatabase() {
+		return;
+	}
+
+}
