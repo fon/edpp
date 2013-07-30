508bff2f42a55dc5c0a1278a020a44d28784417a
fon <fon1989@gmail.com>
2013-07-30 02:15:10 +0100

    Added tests for the MaintenanceTask class

---
+++ edpp/src/test/java/core/MaintenanceTaskTest.java
@@ -0,0 +1,61 @@
+package core;
+
+import static org.junit.Assert.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import network.FakeNode;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import storage.Database;
+import storage.FakeDatabase;
+import util.Phase;
+
+import comm.TransferableMessage;
+
+public class MaintenanceTaskTest {
+
+	static BlockingQueue<TransferableMessage> outQueue;
+	FakeNode localNode;
+	Map<String, Session> sessions;
+	Database db;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		outQueue = new LinkedBlockingQueue<TransferableMessage>();
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		db = new FakeDatabase();
+		localNode = new FakeNode();
+		sessions = new HashMap<String, Session>();
+	}
+	
+	@After
+	public void tearDown() throws Exception {
+		outQueue.clear();
+	}
+
+	@Test
+	public void initPhasePassesWhenNoRemainingTimeIsLeft() throws InterruptedException {
+		Session s = new Session(localNode, 1, 2);
+		Execution e = s.createNewExecution();
+		sessions.put(s.getSessionId(), s);
+		MaintenanceTask mt = new MaintenanceTask(sessions, outQueue, localNode, db);
+		assertTrue(e.remainingInitTime() > 0);
+		assertEquals(Phase.INIT, e.getPhase());
+		mt.run();
+		// When the remaining INIT time is negative, the INIT phase is over
+		assertTrue(e.remainingInitTime() <= 0);
+		assertNotEquals(Phase.INIT, e.getPhase());
+	}
+	
+}
