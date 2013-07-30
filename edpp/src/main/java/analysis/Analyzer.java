40ffa44dac1261d68ef43c3bb3b631b2a8d0a962
fon <fon1989@gmail.com>
2013-07-30 02:14:42 +0100

    .

--- edpp/src/main/java/analysis/Analyzer.java
+++ edpp/src/main/java/analysis/Analyzer.java
@@ -3,8 +3,18 @@ package analysis;
 import java.util.Arrays;
 import java.util.Collections;
 
+/**
+ * Class that provides methods for manipulating and performing computations on eigenvalues
+ * @author Xenofon Foukas
+ *
+ */
 public class Analyzer {
 
+	/**
+	 * Computes the spectral gap from a given array of eigenvalues, where the largest eigenvalue is 1
+	 * @param eigenvalues an array of unsorted eigenvalues
+	 * @return the spectral gap computed by the given eigenvalues
+	 */
 	public static double computeSpectralGap(double [] eigenvalues) {
 		double [] sortedEigenvals = sortEigenvalues(eigenvalues);
 		if (sortedEigenvals.length<2)
@@ -13,6 +23,12 @@ public class Analyzer {
 		return 1 - Math.abs(secondEigenval);
 	}
 	
+	/**
+	 * Computes the mixing time of the eigenvalues derived from a network-related matrix
+	 * @param eigenvalues an array of unsorted eigenvalues
+	 * @param error the accepted error
+	 * @return the mixing time derived by the given eigenvalues
+	 */
 	public static double computeMixingTime(double [] eigenvalues, double error) {
 		double [] sortedEigenvals = sortEigenvalues(eigenvalues);
 		if (sortedEigenvals.length<2)
@@ -21,6 +37,11 @@ public class Analyzer {
 		return Math.log(error) / Math.log(Math.abs(secondEigenval));
 	}
 	
+	/**
+	 * Sorts the given eigenvalues in descending order
+	 * @param eigenvalues an array of eigenvalues
+	 * @return an array with sorted eigenvalues
+	 */
 	public static double [] sortEigenvalues(double [] eigenvalues) {
 		Double [] vals = new Double[eigenvalues.length];
 		for (int i = 0; i < eigenvalues.length; i++) {
