1bfe247ba03b89bf618340d78fdcb1a5f72bfaa7
fon <fon1989@gmail.com>
2013-08-02 00:19:40 +0100

    Improved logging messages in MaintenanceTask class

--- edpp/src/main/java/core/MaintenanceTask.java
+++ edpp/src/main/java/core/MaintenanceTask.java
@@ -9,15 +9,15 @@ import java.util.Map;
 import java.util.concurrent.BlockingQueue;
 import java.util.logging.Logger;
 
-import network.Node;
+import org.jblas.DoubleMatrix;
 
+import network.Node;
 import storage.Database;
 import util.Phase;
 import util.PlainNeighbor;
 import util.PlainNeighborsTable;
 import util.TimedNeighbor;
 import util.TimedNeighborsTable;
-
 import comm.MessageBuilder;
 import comm.MessageSender;
 import comm.ProtocolMessage.SessionEvent;
@@ -59,125 +59,145 @@ public class MaintenanceTask implements Runnable {
 		while (sessionIter.hasNext()) {
 			s = sessionIter.next();
 			//Get all executions of the session
-			logger.info("Checking session "+s.getSessionId());
+//			logger.info("Checking session "+s.getSessionId());
 			//If the session finished, compute the final eigenvalues
 			if (s.hasTerminated()) {
 				logger.info("Session "+s.getSessionId()+" terminated.");
 				sessionIter.remove();
 				RecordedSession recSes = new RecordedSession(s);
 				db.addSession(recSes);
-				System.out.println("Will try to notify listeners");
 				//Notify for the session completion event
 				for (SessionListener sl : sessionListeners) {
 					SessionEvent se = MessageBuilder.buildNewSessionEvent(s, localNode, EventType.TERMINAL);
-					System.out.println("Notifing a listener");
 					sl.sessionCompleted(se);
 				}
-				logger.info("Notified Listeners");
 			}
 			for (int i = 1; i <= s.getCurrentNumberOfExecutions(); i++) {
-				logger.info("Notified Listeners");
 				e = s.getExecution(i);
 				if (e != null){
-					logger.info("Checking execution "+e.getExecutionNumber());
+//					logger.info("Checking execution "+e.getExecutionNumber()+" of session "+s.getSessionId());
 					//Check if it is in the INIT phase
 					if (e.getPhase() == Phase.INIT) {
 						//Check if the INIT phase should end
 						if (e.remainingInitTime() <= 0) {
 							logger.info("Stage is "+e.getPhase()+" and remaining time is "+e.remainingInitTime());
+							logger.info("The INIT phase of Execution "+e.getExecutionNumber()+" in Session "
+									+ s.getSessionId()+" is over. Entering the DATA_EXCHANGE phase");
 							/*
 							 * Enter DATA_EXCHANGE phase
 							 * Go to next round and
 							 * send message to all out-neighbors
 							 */
 							e.setPhase(Phase.DATA_EXCHANGE);
-							logger.info("Stage is now "+Phase.DATA_EXCHANGE);
-							logger.info("The current round is "+e.getCurrentRound());
 							sendOutNextMessage(MessageType.NEXT, s, e);
 							e.recomputeWeight();
 							e.setRound(2);
 							checkForNewExecution(s, e);
-							logger.info("Recomputed weights and set round to "+e.getCurrentRound());
+							logger.info("Recomputed weights and set round to "+e.getCurrentRound()+
+									" in execution "+e.getExecutionNumber()+" of session "+s.getSessionId());
 							if (e.hasAnotherRound()) {
-								logger.fine("This has another round");
 								sendOutNextMessage(MessageType.NEXT, s, e);
 								e.recomputeWeight();
 								e.getInNeighbors().renewTimers();
-								e.setRound(e.getCurrentRound() + 1);
+								int newRound = e.getCurrentRound()+1;
+								e.setRound(newRound);
 								checkForNewExecution(s, e);
+								logger.info("Round "+e.getCurrentRound()+" of execution "+e.getExecutionNumber()
+										+" in session "+s.getSessionId()+" is over. Going to round "+newRound);
 							} else {
-								logger.fine("This was the last round");
+								logger.info("Round "+e.getCurrentRound()+" of execution "+e.getExecutionNumber()
+										+" in session "+s.getSessionId()+" is over. This was the final round. "
+												+ "Switching to GOSSIP phase");
 								e.setPhase(Phase.GOSSIP);
-								logger.info("Time to compute the realization matrix");
-								e.computeRealizationMatrix(localNode.getDiameter());
-								logger.info("Computed the matrix");
-								e.getRealizationMatrix().print();
+								DoubleMatrix rm = e.computeRealizationMatrix(localNode.getDiameter());
+								logger.info("The realization matrix of execution "+e.getExecutionNumber()
+										+" of session "+s.getSessionId()+" was computed");
+								rm.print();
 //								e.getRealizationMatrix().print(NumberFormat.FRACTION_FIELD, 5);
 								//compute the eigenvalues of the approximation matrix
 								//TODO probably should test this for null
 								double [] eigenvals = e.getMatrixAEigenvalues();
+								String eig = "[";
+								for (int j = 0; j < eigenvals.length; j++) {
+									eig+=eigenvals[j];
+									eig+=", ";
+								}
+								eig+="]";
+								logger.info("The computed eigenvalues of execution "+e.getExecutionNumber()
+										+" of session "+s.getSessionId()+" are "+eig);
 								Message msg = MessageBuilder.buildGossipMessage(localNode.getLocalId().toString(),
 										s.getSessionId(), e.getExecutionNumber(), eigenvals);
 								//send GOSSIP message to out-neighbors
 								sendGossipMessage(msg, e);
-								logger.info("Sent the message to all the required nodes");
 							}
 						}
 					} else if (e.getPhase() == Phase.DATA_EXCHANGE) { /* Check for in-neighbors suspected of failure*/
 						inNeighbors = e.getInNeighbors();
 						boolean endOfRound = true;
-						logger.info("Got in neighbors list and now time to check their times");
+//						logger.info("Got in neighbors list and now time to check their times");
 						synchronized (inNeighbors) {
 							Iterator<TimedNeighbor> iter = inNeighbors.iterator();
 //							System.out.println("Making check now");
 							while (iter.hasNext()) {
 								TimedNeighbor neighbor = iter.next();
-								logger.info("Checking neighbor with id "+neighbor.getId().toString());
+//								logger.info("Checking neighbor with id "+neighbor.getId().toString());
 								long remainingTime = neighbor.getTimeToProbe();
 								if (remainingTime != TimedNeighbor.INF)
 									neighbor.decreaseTime(ProtocolController.TIMEOUT);
 								if (neighbor.getTimeToProbe() <= 0) { /* Check whether the node is alive */
-									logger.info("Probing node to see if it is alive");
+//									logger.info("Probing node to see if it is alive");
 									if (isAlive(neighbor)) {
-										logger.info("Node is still alive");
+//										logger.info("Node is still alive");
 										inNeighbors.renewTimer(neighbor);
 										endOfRound = false;
 									} else {
+										logger.info("Node "+neighbor.getId().toString()+" is no longer alive."
+												+ " Removing it from in-neihgbors table of Execution "+
+												e.getExecutionNumber()+" in Session "+s.getSessionId());
 										iter.remove();
 									}
 								} else if (neighbor.getTimeToProbe() != TimedNeighbor.INF) {
 									endOfRound = false;
 								}
 							}
-							System.out.println("Finished with check");
 						}
 						if (endOfRound) {
-							logger.info("Round "+e.getCurrentRound()+" has ended");
 							e.getInNeighbors().renewTimers();
 							// If it is the end of the round check if we have another round
 							if (e.hasAnotherRound()) {
-								logger.fine("This has another round");
 								sendOutNextMessage(MessageType.NEXT, s, e);
 								e.recomputeWeight();
+								int newRound = e.getCurrentRound()+1;
 								e.setRound(e.getCurrentRound() + 1);
 								checkForNewExecution(s, e);
+								logger.info("Round "+e.getCurrentRound()+" of execution "+e.getExecutionNumber()
+										+" in session "+s.getSessionId()+" is over. Going to round "+newRound);
 							} else {
-								logger.fine("This was the last round");
+								logger.info("Round "+e.getCurrentRound()+" of execution "+e.getExecutionNumber()
+										+" in session "+s.getSessionId()+" is over. This was the final round. "
+												+ "Switching to GOSSIP phase");
 								e.setPhase(Phase.GOSSIP);
-								logger.info("Time to compute the realization matrix");
-								e.computeRealizationMatrix(localNode.getDiameter());
-								logger.info("Computed the matrix");
-								e.getRealizationMatrix().print();
+								DoubleMatrix rm = e.computeRealizationMatrix(localNode.getDiameter());
+								logger.info("The realization matrix of execution "+e.getExecutionNumber()
+										+" of session "+s.getSessionId()+" was computed");
+								rm.print();
 								e.transferPendingGossipMessages();
 //								e.getRealizationMatrix().print(NumberFormat.FRACTION_FIELD, 5);
 								//compute the eigenvalues of the approximation matrix
 								//TODO probably should test this for null
 								double [] eigenvals = e.getMatrixAEigenvalues();
+								String eig = "[";
+								for (int j = 0; j < eigenvals.length; j++) {
+									eig+=eigenvals[j];
+									eig+=", ";
+								}
+								eig+="]";
+								logger.info("The computed eigenvalues of execution "+e.getExecutionNumber()
+										+" of session "+s.getSessionId()+" are "+eig);
 								Message msg = MessageBuilder.buildGossipMessage(localNode.getLocalId().toString(),
 										s.getSessionId(), e.getExecutionNumber(), eigenvals);
 								//send GOSSIP message to out-neighbors
 								sendGossipMessage(msg, e);
-								logger.info("Sent the message to all the required nodes");
 							}
 						}
 					} else if (e.getPhase() == Phase.GOSSIP) {
@@ -196,6 +216,9 @@ public class MaintenanceTask implements Runnable {
 										inNeighbors.renewTimer(neighbor);
 										endOfRound = false;
 									} else {
+										logger.info("Node "+neighbor.getId().toString()+" is no longer alive."
+												+ " Removing it from in-neihgbors table of Execution "+
+												e.getExecutionNumber()+" in Session "+s.getSessionId());
 										iter.remove();
 									}
 								} else if (neighbor.getTimeToProbe() != TimedNeighbor.INF) {
@@ -204,11 +227,19 @@ public class MaintenanceTask implements Runnable {
 							}
 						}
 						if (endOfRound) {
-							e.computeMedianEigenvalues();
+							double [] medianEig = e.computeMedianEigenvalues();
+							String eig = "[";
+							for (int j = 0; j < medianEig.length; j++) {
+								eig+=medianEig[j];
+								eig+=", ";
+							}
+							eig+="]";
+							logger.info("Gossip round of execution "+e.getExecutionNumber()+" of session "
+									+s.getSessionId()+" is now over. The median of all received eigenvalues is "+
+									eig);
 							e.setPhase(Phase.TERMINATED);
 							s.addCompletedExecution();
 							if (s.hasTerminated()) {
-								System.out.println("Update database");
 								sessionIter.remove();
 								RecordedSession recSes = new RecordedSession(s);
 								db.addSession(recSes);
@@ -223,7 +254,6 @@ public class MaintenanceTask implements Runnable {
 					}
 				}
 			}
-			logger.info("Notified Listeners");
 		}
 	}
 	
@@ -258,7 +288,6 @@ public class MaintenanceTask implements Runnable {
 	private void checkForNewExecution(Session s, Execution e) {
 		if (s.isInitiator() && e.equals(s.getInitExecution())
 				&& s.newExecutionExpected()) {
-			logger.info("Initiating a new execution");
 			Execution newExecution;
 			newExecution = s.createNewExecution();
 			
@@ -273,8 +302,9 @@ public class MaintenanceTask implements Runnable {
 				for (PlainNeighbor n : pnt) {
 					address = n.getAddress();
 					valueToSend = newExecution.getCurrentValue() * n.getWeight();
-					logger.info("Sending INIT message to "+n.getId()+" with address "+address);
-					logger.info("The current node is "+nodeId);
+					logger.info("Sending INIT message to node "+n.getId()+" with address "+address+
+							" for execution "+newExecution.getExecutionNumber()+
+							" of session "+sessionId);
 					outMessage = MessageBuilder.buildInitMessage(nodeId, sessionId, 
 								newExecution.getExecutionNumber(), s.getNumberOfExecutions(),
 								s.getNumberOfRounds(), valueToSend);
@@ -298,9 +328,13 @@ public class MaintenanceTask implements Runnable {
 				valueToSend = execution.getCurrentValue() * n.getWeight();
 				//round number + 1, because we send the message for a round
 				//using the value of the previous round
+				int r = execution.getCurrentRound() + 1;
+				logger.info("Sending NEXT message to node "+n.getId()+" with address "+address+
+						" for round "+r+" of execution "+execution.getExecutionNumber()+
+						" in session "+sessionId);
 				outMessage = MessageBuilder.buildNextMessage(nodeId,
 						sessionId, execution.getExecutionNumber(), 
-						execution.getCurrentRound() + 1, valueToSend);
+						r, valueToSend);
 				outgoingQueue.add(new TransferableMessage(outMessage, address));
 			}
 		}
