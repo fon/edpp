22457691347e3127445c0ef188872b86bf388035
fon <fon1989@gmail.com>
2013-09-03 23:09:48 +0100

    Performed code cleanups in TransferableMessage class

--- edpp/src/main/java/comm/TransferableMessage.java
+++ edpp/src/main/java/comm/TransferableMessage.java
@@ -6,26 +6,31 @@ import comm.ProtocolMessage.Message;
 
 /**
  * A protocol message along with the destination address
+ * 
  * @author Xenofon Foukas
- *
+ * 
  */
 public class TransferableMessage {
-	
+
 	private Message message;
 	private InetAddress address;
 	private boolean sendReliably;
 
 	/**
 	 * Class constructor
-	 * @param message The Message that will be sent to the remote node
-	 * @param address The InetAddress of the remote node
+	 * 
+	 * @param message
+	 *            The Message that will be sent to the remote node
+	 * @param address
+	 *            The InetAddress of the remote node
 	 */
-	public TransferableMessage(Message message, InetAddress address, boolean sendReliably) {
+	public TransferableMessage(Message message, InetAddress address,
+			boolean sendReliably) {
 		this.setMessage(message);
 		this.address = address;
 		this.setSendReliably(sendReliably);
 	}
-	
+
 	public TransferableMessage(Message message, InetAddress address) {
 		this.setMessage(message);
 		this.address = address;
@@ -42,12 +47,13 @@ public class TransferableMessage {
 
 	/**
 	 * 
-	 * @param message The Message to be transfered
+	 * @param message
+	 *            The Message to be transfered
 	 */
 	public void setMessage(Message message) {
 		this.message = message;
 	}
-	
+
 	/**
 	 * 
 	 * @return The InetAddress of the remote node
@@ -58,18 +64,32 @@ public class TransferableMessage {
 
 	/**
 	 * 
-	 * @param address The InetAddress of the remote node
+	 * @param address
+	 *            The InetAddress of the remote node
 	 */
 	public void setAddress(InetAddress address) {
 		this.address = address;
 	}
 
+	/**
+	 * Returns the reliability level that needs to be used for sending the
+	 * message
+	 * 
+	 * @return true if the message should be sent through TCP, false for UDP
+	 */
 	public boolean getSendReliably() {
 		return sendReliably;
 	}
 
+	/**
+	 * Sets the reliability level that needs to be used for sending the message
+	 * 
+	 * @param sendReliably
+	 *            this should be true if the message should be sent through TCP
+	 *            and false for UDP
+	 */
 	public void setSendReliably(boolean sendReliably) {
 		this.sendReliably = sendReliably;
 	}
-	
+
 }
