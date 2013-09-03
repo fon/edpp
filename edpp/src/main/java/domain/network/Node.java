030f4b234fe4235dd2059a2921c1edaaad9d757b
fon <fon1989@gmail.com>
2013-09-04 00:49:06 +0100

    Performed code cleanups in the Node interface

--- edpp/src/main/java/domain/network/Node.java
+++ edpp/src/main/java/domain/network/Node.java
@@ -5,27 +5,37 @@ import java.util.Set;
 import domain.Id;
 import domain.Neighbor;
 
+/**
+ * Interface for providing an abstract representation of the underlying network
+ * to the top layer of the protocol
+ * 
+ * @author Xenofon Foukas
+ * 
+ */
 public interface Node {
 
 	/**
-	 * This method is responsible for providing the higher protocol
-	 * layer with a set containing the local node's
-	 * out-neighbors
+	 * This method is responsible for providing the higher protocol layer with a
+	 * set containing the local node's out-neighbors
+	 * 
 	 * @return A set containing the local node's out-neighbors
 	 */
 	public Set<Neighbor> getOutNeighbors();
-	
+
 	/**
-	 * This method returns the Id of the local node 
+	 * This method returns the Id of the local node
+	 * 
 	 * @return
 	 */
 	public Id getLocalId();
-	
+
 	/**
-	 * Optional method that provides a hint to the underlying
-	 * network for a failed node which should be removed. 
-	 * The network can choose to accept or ignore this hint
-	 * @param id The id of the failed node in a String format
+	 * Optional method that provides a hint to the underlying network for a
+	 * failed node which should be removed. The network can choose to accept or
+	 * ignore this hint
+	 * 
+	 * @param id
+	 *            The id of the failed node in a String format
 	 * @return true if the network actually removes the node, otherwise false
 	 */
 	public boolean removeOutNeighborNode(String id);
