fb197e02b1865cf70800809b5825db734ff77e8f
fon <fon1989@gmail.com>
2013-09-04 00:20:26 +0100

    Performed code cleanups in the KeyValueDatabase class

--- edpp/src/main/java/storage/KeyValueDatabase.java
+++ edpp/src/main/java/storage/KeyValueDatabase.java
@@ -13,42 +13,65 @@ import jdbm.RecordManagerFactory;
 import domain.RecordedSession;
 import event.SessionListener;
 
+/**
+ * Class for providing storage capabilities to the protocol using key-value
+ * pairs. The underlying storage mechanism used is jdbm2
+ * 
+ * @author Xenofon Foukas
+ * 
+ */
 public class KeyValueDatabase implements Database {
 
 	private RecordManager recMan;
 	private PrimaryTreeMap<Integer, RecordedSession> db;
-	
+
 	private final Object lock;
-	
+
 	private List<SessionListener> sessionListeners;
-	
+
+	/**
+	 * Constructor class
+	 * 
+	 * @param dbName
+	 *            A string with the name of the database where the sessions are
+	 *            stored. If no database exists with that name, it will be
+	 *            created
+	 * @param tableName
+	 *            A string with the name of the table in the database for
+	 *            storing completed Session information. If such a table does
+	 *            not exist, it will be created
+	 */
 	public KeyValueDatabase(String dbName, String tableName) {
-		sessionListeners  = Collections.synchronizedList(new ArrayList<SessionListener>());
+		sessionListeners = Collections
+				.synchronizedList(new ArrayList<SessionListener>());
 		lock = new Object();
 		try {
 			recMan = RecordManagerFactory.createRecordManager(dbName);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		db = recMan.treeMap(tableName);
 	}
-	
+
 	@Override
 	public void addSession(RecordedSession rs) {
 		int size;
 		synchronized (lock) {
 			try {
-				size = db.lastKey()+1;
+				// Compute the index of the new record by finding the index of
+				// the last stored session and adding one to it
+				size = db.lastKey() + 1;
 			} catch (NoSuchElementException nse) {
+				// If no previous record is found, the first index will be 0
 				size = 0;
 			}
 			db.put(new Integer(size), rs);
 		}
+		// Notify all listeners that a new record was inserted
 		synchronized (sessionListeners) {
 			Iterator<SessionListener> iter = sessionListeners.iterator();
-			while(iter.hasNext()) {
+			while (iter.hasNext()) {
 				iter.next().sessionStored(rs);
 			}
 		}
@@ -60,8 +83,10 @@ public class KeyValueDatabase implements Database {
 		RecordedSession rs = null;
 		synchronized (lock) {
 			try {
+				// Get the index of the last recorded session
 				lastRecord = db.lastKey();
 			} catch (NoSuchElementException e) {
+				// If no index is found, set it to 0
 				lastRecord = 0;
 			}
 			try {
@@ -73,12 +98,12 @@ public class KeyValueDatabase implements Database {
 		}
 		return rs;
 	}
-	
+
 	@Override
 	public synchronized void addSessionListener(SessionListener listener) {
 		sessionListeners.add(listener);
 	}
-	
+
 	@Override
 	public synchronized boolean removeSessionListener(SessionListener listener) {
 		return sessionListeners.remove(listener);
@@ -87,6 +112,7 @@ public class KeyValueDatabase implements Database {
 	@Override
 	public void closeDatabase() {
 		try {
+			// commit any changes before closing the database
 			recMan.commit();
 			recMan.close();
 		} catch (IOException e) {
@@ -94,6 +120,5 @@ public class KeyValueDatabase implements Database {
 			e.printStackTrace();
 		}
 	}
-	
 
 }
