9fb16c3a12bd0e9408317e1c89f2ea0c3fc30393
fon <fon1989@gmail.com>
2013-09-04 22:20:56 +0100

    Performed code cleanups in RecordedSession class

--- edpp/src/main/java/domain/RecordedSession.java
+++ edpp/src/main/java/domain/RecordedSession.java
@@ -2,7 +2,15 @@ package domain;
 
 import java.io.Serializable;
 
-public class RecordedSession implements Serializable{
+/**
+ * Class storing a terminated Session and the time of termination. When an
+ * object of this class is created, a timestamp is stored indicating the time
+ * the event occurred
+ * 
+ * @author Xenofon Foukas
+ * 
+ */
+public class RecordedSession implements Serializable {
 
 	/**
 	 * 
@@ -10,18 +18,33 @@ public class RecordedSession implements Serializable{
 	private static final long serialVersionUID = -8424294980274800118L;
 	private final long timestamp;
 	private final Session recordedSession;
-	
+
+	/**
+	 * Class constructor
+	 * 
+	 * @param recordedSession
+	 *            the terminated Session object
+	 */
 	public RecordedSession(Session recordedSession) {
 		timestamp = System.currentTimeMillis();
 		this.recordedSession = recordedSession;
 	}
-	
+
+	/**
+	 * 
+	 * @return Returns the terminated Session object
+	 */
 	public Session getRecordedSession() {
 		return recordedSession;
 	}
-	
+
+	/**
+	 * 
+	 * @return the timestamp of when the Session object was stored, i.e. the
+	 *         timestamp of when this object was created
+	 */
 	public long getTimestamp() {
 		return timestamp;
 	}
-	
+
 }
