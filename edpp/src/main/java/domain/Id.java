5916d081134de868cb7a962e26d4f6d4992dadc6
fon <fon1989@gmail.com>
2013-09-04 01:46:53 +0100

    Performed code cleanups in the Id class

--- edpp/src/main/java/domain/Id.java
+++ edpp/src/main/java/domain/Id.java
@@ -4,57 +4,92 @@ import java.util.Arrays;
 
 import org.apache.commons.codec.binary.Base64;
 
+/**
+ * Class for wrapping the ids used by the underlying network for identifying the
+ * nodes
+ * 
+ * @author Xenofon Foukas
+ * 
+ */
 public class Id {
-	
-	private byte [] id;
-	
+
+	private byte[] id;
+
+	/**
+	 * Copy constructor
+	 * 
+	 * @param id
+	 *            the id to be copied to the new object
+	 */
 	public Id(Id id) {
 		this.id = id.getByteRepresentation();
 	}
-	
-	public Id(byte [] id) {
+
+	/**
+	 * Class constructor
+	 * 
+	 * @param id
+	 *            the byte representation of the new Id
+	 */
+	public Id(byte[] id) {
 		this.id = id.clone();
 	}
-	
+
+	/**
+	 * Class constructor
+	 * 
+	 * @param stringId
+	 *            the string representation of the new Id
+	 */
 	public Id(String stringId) {
 		this.id = getByteIdFromString(stringId);
 	}
-	
-	public byte [] getByteRepresentation() {
+
+	/**
+	 * 
+	 * @return a byte array containing this Id
+	 */
+	public byte[] getByteRepresentation() {
 		return id;
 	}
-	
+
 	@Override
 	public String toString() {
 		return getStringId(this.id);
 	}
-	
+
 	@Override
 	public boolean equals(Object obj) {
-		if (obj==this) return true;
-		if(!(obj instanceof Id)) return false;
-		Id i = (Id)obj;
-		
+		if (obj == this)
+			return true;
+		if (!(obj instanceof Id))
+			return false;
+		Id i = (Id) obj;
+
 		return Arrays.equals(id, i.getByteRepresentation());
-		
+
 	}
-	
+
 	/**
 	 * Converts a base64 string to a byte array representation of the Id
-	 * @param stringId The base64 representation of the Id
+	 * 
+	 * @param stringId
+	 *            The base64 representation of the Id
 	 * @return A byte array of the Id
 	 */
-	public static byte [] getByteIdFromString(String stringId) {
-		byte [] byteId = Base64.decodeBase64(stringId);
+	public static byte[] getByteIdFromString(String stringId) {
+		byte[] byteId = Base64.decodeBase64(stringId);
 		return byteId;
 	}
-	
+
 	/**
 	 * Converts a byte array representation of the Id to a base 64 string
-	 * @param byteId The byte array representation of the Id
+	 * 
+	 * @param byteId
+	 *            The byte array representation of the Id
 	 * @return A base64 string representation
 	 */
-	public static String getStringId(byte [] byteId) {
+	public static String getStringId(byte[] byteId) {
 		String stringId = new String(Base64.encodeBase64(byteId));
 		return stringId;
 	}
